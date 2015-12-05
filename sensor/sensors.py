7867550b7add782425cd154ef371eef8daf4ac81
Teemu Partanen <teemupart@gmail.com>
2015-12-05 17:03:19 +0000

    Sensor API

---
+++ sensor/sensors.py
@@ -0,0 +1,24 @@
+import Adafruit_DHT
+
+DHT_TYPE    = Adafruit_DHT.AM2302
+AM2302_PIN  = 4
+
+class Sensors(object):
+    '''API to access connected sensors'''
+    def __init__(self):
+        super(Sensors, self).__init__()
+
+def get_temp_and_hum():
+    '''
+    Read temperature and humidity values from the AM2302 sensor
+    IN: -
+    OUT:    if success, return values
+            else, return None
+    '''
+    humidity, temperature = Adafruit_DHT.read(DHT_TYPE, AM2302_PIN)
+    if temperature is not None and humidity is not None:
+        values = {'temperature':temperature, 'humidity':humidity}
+        return values
+
+    else:
+        return None
