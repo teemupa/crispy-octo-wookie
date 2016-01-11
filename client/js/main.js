9e67f7cd4d4144e8611f6c5f6981300fb026b188
Miika Keisu <mikeisu@student.oulu.fi>
2016-01-11 19:36:51 +0200

    client web app almost finished

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,65 @@
 $(function(){
+
+
+    function alertIfNeeded( temp ) {
+
+        if (temp < 20)
+        {
+            $('#myModal').modal();
+            $('#myModal').modal('show');
+        }
+
+
+    }
+
+
     var navMain = $("#navbar");
     navMain.on("click", "a", null, function () {
         navMain.collapse('hide');
     });
 
-    var api = "http://teemupa.dy.fi:5000/api/";
 
+    // Generate bar graph
+    var ctx1 = document.getElementById("humidity-canvas").getContext("2d");
+    var barChartData = {
+        labels : [],
+        datasets : [
+            {
+                label: "Humidity data",
+                fillColor : "rgba(151,187,205,0.5)",
+                strokeColor : "rgba(151,187,205,0.8)",
+                highlightFill : "rgba(151,187,205,0.75)",
+                highlightStroke : "rgba(151,187,205,1)",
+                data : []
+            }
+        ]
+    }
+
+    var barChart = new Chart(ctx1).Bar(barChartData, {
+        responsive : true
+    });
+
+    // Generate bar graph
+    var ctx2 = document.getElementById("temperature-canvas").getContext("2d");
+    var lineChartData = {
+        labels : [],
+        datasets : [
+            {
+                label: "Temperature data",
+                fillColor : "rgba(151,187,205,0.5)",
+                strokeColor : "rgba(151,187,205,0.8)",
+                highlightFill : "rgba(151,187,205,0.75)",
+                highlightStroke : "rgba(151,187,205,1)",
+                data : []
+            }
+        ]
+    }
+
+    var lineChart = new Chart(ctx2).Line(lineChartData, {
+        responsive : true
+    });
+
+    var api = "http://teemupa.dy.fi:5000/api/";
 
     $.getJSON( api, function(data) {
 	
@@ -19,6 +73,7 @@ $(function(){
 		$("#current-temperature").text(cur_temp);
 		$("#current-humidity").text(cur_hum);
 		$("#timestamp").text(cur_time);
+        alertIfNeeded( cur_temp );
 		
     })
         .done(function() {
@@ -36,11 +91,21 @@ $(function(){
 	
 
 		var historyArray = data["collection"]["items"];
-		var historyLenght = historyArray.length;
-	
-		for (var i = 0; i < historyLenght; i++) {
-			console.log(historyArray[i]["timestamp"] + " Temp: " + historyArray[i]["temp_in"] + " Hum: " + historyArray[i]["hum"]);
-		}	
+		var historyLength = historyArray.length;
+
+        var humidityVal, temperatureVal;
+		for (var i = 0; i < historyLength; i++) {
+            humidityVal = Math.round(historyArray[i]["hum"] * 100) / 100;
+            temperatureVal = Math.round(historyArray[i]["temp_in"] * 100) / 100
+            console.log(historyArray[i]["timestamp"] + " Temp: " + historyArray[i]["temp_in"] + " Hum: " + historyArray[i]["hum"]);
+            barChart.addData([humidityVal], i-historyLength);
+            lineChart.addData([temperatureVal], i-historyLength);
+            $(".loader").hide();
+            $("canvas").show();
+            barChart.update();
+            $(window).resize();
+        }
+
     })
         .done(function() {
             console.log( "second success" );
@@ -52,8 +117,4 @@ $(function(){
             console.log( "complete" );
         });
 
-	
-	
-	
-		
 });
